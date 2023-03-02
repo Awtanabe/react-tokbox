39ab21e894bb161860da4ef07e82716fbc97bafc
akifumi_watanabe <akifumi_watanabe@akifumi-watanabenonotopasokon.local>
2023-03-02 18:43:07 +0900

    UI修正

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React,{ useState, useEffect} from 'react'
 import { OTSession, OTPublisher, OTStreams, OTSubscriber, createSession } from 'opentok-react';
+import { extendTheme, ChakraProvider } from '@chakra-ui/react'
+import styles from 'styled-components';
 
+const Container = styles.div`
+  width: 800px;
+  margin: 0 auto;
+  padding-top: 100px;
+`
 
 function App() {
   const [ streams, setStreams ] = useState([])
@@ -31,26 +38,28 @@ function App() {
   );
 
     return (
-      <div>
-      {session && (
-      <OTSession
-      apiKey={process.env.REACT_APP_TOKBOX_API_KEY}
-      sessionId={process.env.REACT_APP_TOKBOX_SESSION_ID}
-      token={process.env.REACT_APP_TOKBOX_TOKEN}
-    >
-      <OTPublisher
-      session={session.session}
-       />
-      <OTStreams>
-          <OTSubscriber
-            properties={{ showControls: false }}
-            session={session.session}
-            stream={streams}
-          />
-      </OTStreams>
-    </OTSession>
-      )}
-    </div>
+      <Container>
+        {session && (
+        <OTSession
+        apiKey={process.env.REACT_APP_TOKBOX_API_KEY}
+        sessionId={process.env.REACT_APP_TOKBOX_SESSION_ID}
+        token={process.env.REACT_APP_TOKBOX_TOKEN}
+      >
+        <OTPublisher
+        session={session.session}
+        properties={{  width: 700, height: 500}}
+        />
+        <OTStreams>
+            <OTSubscriber
+              properties={{ showControls: false, insertMode: 'append' }}
+              session={session.session}
+              stream={streams}
+            />
+        </OTStreams>
+      </OTSession>
+        )}
+      </Container>
+
     )
 }
 
