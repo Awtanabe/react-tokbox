eac5c355942eaa092d44b7c83b7c9497a3aa1553
akifumi_watanabe <akifumi_watanabe@akifumi-watanabenonotopasokon.local>
2023-03-02 18:09:19 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{ useState, useEffect} from 'react'
+import { OTSession, OTPublisher, OTStreams, OTSubscriber, createSession } from 'opentok-react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [ streams, setStreams ] = useState([])
+  const [session, setSession] = useState(undefined)
+
+  useEffect(() => {
+    if(!session) {
+      const sessionHelper = createSession({
+        apiKey: process.env.REACT_APP_TOKBOX_API_KEY,
+        sessionId: process.env.REACT_APP_TOKBOX_SESSION_ID,
+        token: process.env.REACT_APP_TOKBOX_TOKEN,
+        onStreamsUpdated: streams => setStreams(streams[0])
+      })
+      setSession(sessionHelper)
+  
+    }
+  })
+
+  useEffect(
+    () => () => {
+      if (session) {
+        session.disconnect();
+      }
+    },
+    [session],
   );
+
+    return (
+      <div>
+      {session && (
+      <OTSession
+      apiKey={process.env.REACT_APP_TOKBOX_API_KEY}
+      sessionId={process.env.REACT_APP_TOKBOX_SESSION_ID}
+      token={process.env.REACT_APP_TOKBOX_TOKEN}
+    >
+      <OTPublisher
+      session={session.session}
+       />
+      <OTStreams>
+          <OTSubscriber
+            properties={{ showControls: false }}
+            session={session.session}
+            stream={streams}
+          />
+      </OTStreams>
+    </OTSession>
+      )}
+    </div>
+    )
 }
 
-export default App;
+export default App
